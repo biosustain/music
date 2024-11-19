eae0e1f9794fbf7ceeb0f0e94908b3075b023207
Shahaf <118754339+Deusxy@users.noreply.github.com>
2024-11-19 10:59:23 +0000

    lib changes

--- lib/global_constants.py
+++ lib/global_constants.py
@@ -1,16 +1,18 @@
-__author__ = 'kaizhuang'
+__author__ = 'Shahaf Cohen'
 
-####################
 # Global Variables #
-####################
-from framed.bioreactor import AEROBIC, ANAEROBIC
+from cobra import Model, Reaction
 
-# reaction names in metabolic models
-r_glc = 'R_EX_glc_e'
-r_glyc = 'R_EX_glyc_e'
-r_pdo = 'R_EX_1_3_pdo_e'
-r_3hp = 'R_EX_3_hp_e'
-r_o2 = 'R_EX_o2_e'
+# Define reactions
+r_glc = 'EX_glc_e'
+r_glyc = 'EX_glyc_e'
+r_pdo = 'EX_1_3_pdo_e'
+r_3hp = 'EX_3_hp_e'
+r_o2 = 'EX_o2_e'
+
+# Define conditions
+AEROBIC = 'aerobic'
+ANAEROBIC = 'anaerobic'
 
 conditions = [AEROBIC, ANAEROBIC]
 substrates = [r_glc, r_glyc]
